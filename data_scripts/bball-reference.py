85b8b2cefae1670e9aaf374dcb36ef2857fe3a45
kagrawal7 <kush.agr02@gmail.com>
2024-03-22 20:25:45 -0400

    new csv file exclused most players with low field goal attempts

--- data_scripts/bball-reference.py
+++ data_scripts/bball-reference.py
@@ -1,6 +1,25 @@
 import pandas as pd
 
+# min. field goal attempts per game
+FGA_THRESHOLD = 10
+
 
 
 if __name__ == '__main__':
-    df = pd.read_csv("C:\Users\kusha\OneDrive\Desktop\Uni\Winter_2024\STA365\STA365-Final-Project\shot_logs_final_v2.csv")
+    df = pd.read_csv("C:/Users/kusha/OneDrive/Desktop/Uni/Winter_2024/STA365/STA365-Final-Project/data_files/shot_logs_final_v2.csv")
+    fga_df = pd.read_csv("C:/Users/kusha/OneDrive/Desktop/Uni/Winter_2024/STA365/STA365-Final-Project//data_files/fga_sort.csv", encoding_errors='ignore')
+    weird_names = []
+    # figure out ? in player names
+    players = fga_df["Player"][fga_df["FGA"] < FGA_THRESHOLD]
+    for player in players:
+        try:
+            df = df.drop(df[df["player_name"] == player].index)
+            print(player)
+        except:
+            weird_names.append(player)
+            print(player)
+            continue
+        finally:
+            df.to_csv("C:/Users/kusha/OneDrive/Desktop/Uni/Winter_2024/STA365/STA365-Final-Project/data_files/players_are_filtered.csv")
+        
+    # print(fga_df["Player"][20])
